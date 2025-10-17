6bbde6ee2ae91a2c91981704ffe82218e93e695e
ryoryoryo3838 <miya.klis.default@gmail.com>
2025-10-17 17:05:23 +0900

    init

---
+++ install.sh
@@ -0,0 +1,25 @@
+### DEVBOX setting
+# devbox check
+if [ ! $(command -v devbox ) ]; then
+  echo "devbox not installed"
+  exit 1
+fi
+
+# sym global config
+DEV_GLOBAL_DIR="$HOME/.local/share/"
+
+rm "$DEV_GLOBAL_DIR/devbox" -r
+
+ln -sf $HOME/dotfiles/devbox $DEV_GLOBAL_DIR
+
+### Shell
+ln -sf $HOME/dotfiles/zsh/zshrc $HOME/.zshrc
+# sheldon
+ln -sf $HOME/dotfiles/sheldon/ $HOME/.config/
+
+# source $HOME/.zshrc
+# refresh-global
+
+### symlink
+# .config
+stow -d $HOME/dotfiles -t $HOME/.config/ config
---
+++ readme.md
@@ -0,0 +1,8 @@
+# 構成要素
+
+- devbox: globalなpmとして使う。ディレクトリごとにパッケージ管理も行い環境を分離する
+- sheldon: シェル管理
+
+# pre requirement
+- zsh(bluefin already installed)
+- devbox(安全面が不安なので手動で)
