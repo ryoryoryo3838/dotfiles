6677b6abdc154e2fb7cbcd24624a4e3efe25b6a4
ryoryoryo3838 <rseimiya@miya-lis.net>
2026-04-25 18:21:30 +0900

    add ignore: devbox.lock, some nvim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,22 +1,17 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- <leader> key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set to true if you have a Nerd Font installed and selected in the terminal
+-- nerd font applying
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-- NOTE:[[ Setting options ]]
 
--- Make line numbers default
+-- line number
 vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+
+-- relative number
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
